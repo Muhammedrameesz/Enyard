9fcb53aad81c1a35a5ffc2bb3b0992494686e76f
Muhammedrameesz <rameezta555@gmail.com>
2025-02-12 22:03:36 +0530

    new

--- src/components/TimeTick/SquareCards.jsx
+++ src/components/TimeTick/SquareCards.jsx
@@ -93,10 +93,10 @@ export default function SquareCards() {
                   <h1
                     className={`text-gray-600 font-medium
                     ${
-                      i < 2
-                        ? "text-gray-600 group-hover:text-gray-50 transition-colors duration-300"
-                        : "text-gray-50"
-                    }
+                      i < 2 
+                        ? "text-gray-600 group-hover:text-gray-100 transition-colors duration-300"
+                        : "text-gray-100"
+                    }  
                     `}
                   >
                     {item.subtitle}
